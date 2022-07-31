eef48eb9a63d1917c1ee8738d3d6e538f344f1ec
kadurin-b <kadurin.b@gmail.com>
2022-07-31 17:05:24 +0200

    test commit

---
+++ react-todo-app/src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Counter from './Counter';
+
+function App() {
+  return (
+    <div className="container">
+      <header className="header">
+        <h1 className="">Hello app</h1>
+      </header>
+      <main className="App-main">
+        <Counter />
+      </main>
+    </div>
+  );
+}
+
+export default App;
+
---
+++ react-todo-app/src/Counter.js
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+function Counter() {
+  const [counter, setCounter] = useState(0);
+
+  const onClickCounterPlus = () => {
+    setCounter(counter + 1);
+
+    
+  };
+  const onClickCounterMinus = () => {
+    setCounter(counter - 1);
+  };
+
+  return (
+    <div>
+      <button onClick={onClickCounterPlus} className="plus">
+        +1 Button
+      </button>
+      <button onClick={onClickCounterMinus} className="minus">
+        -1 Button
+      </button>
+      {counter}
+    </div>
+  );
+}
+
+export default Counter;
